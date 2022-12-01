3e779af524f3f6ad7cadfa096c920ab0bf2b7485
John Wirth <jjwirth2000@gmail.com>
2022-12-01 09:56:21 -0600

    Fixed bug where scripts weren't different between multiplayer and single player

--- Assets/Scripts/SinglePlayer/UpdateSprite_SP.cs
+++ Assets/Scripts/SinglePlayer/UpdateSprite_SP.cs
@@ -2,23 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UpdateSprite : MonoBehaviour
+public class UpdateSprite_SP : MonoBehaviour
 {
     public Sprite cardFace;
     public Sprite cardBack;
     private SpriteRenderer spriteRenderer;
     private Selectable selectable;
-    private Game game;
-    private UserInput userInput;
+    private Game_SP game;
+    private UserInput_SP userInput;
     private ToggleCamera toggleCamera;
     
     // Start is called before the first frame update
     void Start()
     {
-        List<string> playerDeck = Game.GenerateDeck();
-        List<string> computerDeck = Game.GenerateDeck(false);
-        game = FindObjectOfType<Game>();
-        userInput = FindObjectOfType<UserInput>();
+        List<string> playerDeck = Game_SP.GenerateDeck();
+        List<string> computerDeck = Game_SP.GenerateDeck(false);
+        game = FindObjectOfType<Game_SP>();
+        userInput = FindObjectOfType<UserInput_SP>();
         Vector3 scale = new Vector3(0.33f, 0.33f, 1.0f);
 
         int i = 0;
